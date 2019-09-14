2347c231c8da4944e0bf2ef63103798395c69bcd
adrianstevens <adrian.stevens@gmail.com>
2019-09-14 14:01:44 -0700

    Extension 0.2.2 update

---
+++ CreateAddinPackage.sh
@@ -0,0 +1,1 @@
+mono "/Applications/Visual Studio.app/Contents/Resources/lib/monodevelop/bin/vstool.exe" setup pack ./VS4Mac_Meadow_Extension/bin/Release/net471/Meadow.Sdks.IdeExtensions.Vs4Mac.dll
--- VS4Mac_Meadow_Extension/Properties/AddinInfo.cs
+++ VS4Mac_Meadow_Extension/Properties/AddinInfo.cs
@@ -5,10 +5,10 @@ using Mono.Addins.Description;
 [assembly: Addin(
     "Meadow",
     Namespace = "WildernessLabs.Sdks",
-    Version = "0.2.0"
+    Version = "0.2.2"
 )]
 
 [assembly: AddinName("Meadow")]
 [assembly: AddinCategory("IDE extensions")]
 [assembly: AddinDescription("Build and deployment tools for Meadow by Wilderness Labs")]
-[assembly: AddinAuthor("Bryan Costanich, Adrian Stevens")]
+[assembly: AddinAuthor("Bryan Costanich, Adrian Stevens, Brian Kim")]
