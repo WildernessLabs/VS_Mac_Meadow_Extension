2309d59d7b56d0ee3f433617a10ee48e79137eaa
adrianstevens <adrian.stevens@gmail.com>
2019-10-27 20:50:19 -0700

    Update package creation script for .NET 4.7.2

--- CreateAddinPackage.sh
+++ CreateAddinPackage.sh
@@ -1,1 +1,1 @@
-mono "/Applications/Visual Studio.app/Contents/Resources/lib/monodevelop/bin/vstool.exe" setup pack ./VS4Mac_Meadow_Extension/bin/Release/net471/Meadow.Sdks.IdeExtensions.Vs4Mac.dll
+mono "/Applications/Visual Studio.app/Contents/Resources/lib/monodevelop/bin/vstool.exe" setup pack ./VS4Mac_Meadow_Extension/bin/Release/net472/Meadow.Sdks.IdeExtensions.Vs4Mac.dll
