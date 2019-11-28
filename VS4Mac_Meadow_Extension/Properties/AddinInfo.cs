0d7eafc16bdb49cca3d5eaded11a60fc149a061f
Adrian Stevens <adrian.stevens@gmail.com>
2019-11-28 08:53:43 -0800

    VS4Mac b3.6 udpates

--- VS4Mac_Meadow_Extension/Properties/AddinInfo.cs
+++ VS4Mac_Meadow_Extension/Properties/AddinInfo.cs
@@ -5,7 +5,7 @@ using Mono.Addins.Description;
 [assembly: Addin(
     "Meadow",
     Namespace = "WildernessLabs.Sdks",
-    Version = "0.4.3"
+    Version = "0.5.0"
 )]
 
 [assembly: AddinName("Meadow")]
