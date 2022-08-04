c025280c1ded9eb8796d95fdad7aa4985217dcd5
Dominique Louis <savagesoftware@gmail.com>
2022-08-04 14:11:13 +0200

    Add command shell script for VS2022 too, and tweak so we build from the csproj, rather than slns.

--- build_package_2019.sh
+++ build_package_2019.sh
@@ -1,8 +1,8 @@
 mv "/Applications/Visual Studio.app" "/Applications/Visual Studio 2022.app"
 mv "/Applications/Visual Studio 2019.app" "/Applications/Visual Studio.app"
 
-msbuild VS4Mac_Meadow_Extension.sln /t:Restore /p:Configuration=Release
-msbuild VS4Mac_Meadow_Extension.sln /t:Build /p:Configuration=Release /p:CreatePackage=true
+msbuild VS4Mac_Meadow_Extension/Meadow.Sdks.IdeExtensions.Vs4Mac.csproj	 /t:Restore /p:Configuration=Release
+msbuild VS4Mac_Meadow_Extension/Meadow.Sdks.IdeExtensions.Vs4Mac.csproj	 /t:Build /p:Configuration=Release /p:CreatePackage=true
 
 mv "/Applications/Visual Studio.app" "/Applications/Visual Studio 2019.app"
 mv "/Applications/Visual Studio 2022.app" "/Applications/Visual Studio.app"
