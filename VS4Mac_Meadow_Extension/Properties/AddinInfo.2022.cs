f9f3d2be02f40750d5f8f7e012ee9d695305b8ea
Dominique Louis <savagesoftware@gmail.com>
2022-10-22 12:52:58 +0200

    Bump Addin version to RC1 version of 0.90.0 (#27)
    * Bump Addin version to RC1 version of 0.90.0

--- VS4Mac_Meadow_Extension/Properties/AddinInfo.2022.cs
+++ VS4Mac_Meadow_Extension/Properties/AddinInfo.2022.cs
@@ -4,7 +4,7 @@ using Mono.Addins.Description;
 [assembly: Addin(
     "Meadow.2022",
     Namespace = "WildernessLabs.Sdks",
-    Version = "0.19.14"
+    Version = "0.90.0"
 )]
 
 [assembly: AddinName("Meadow for VS2022")]
