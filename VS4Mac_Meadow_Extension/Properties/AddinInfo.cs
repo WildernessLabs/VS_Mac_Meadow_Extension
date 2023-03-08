165a316e96d52775d36ea9226fb2cc93610d7f88
Dominique Louis <savagesoftware@gmail.com>
2023-03-08 12:55:08 +0000

    Bump extension versions to 0.96.0 for RC3.

--- VS4Mac_Meadow_Extension/Properties/AddinInfo.cs
+++ VS4Mac_Meadow_Extension/Properties/AddinInfo.cs
@@ -4,7 +4,7 @@ using Mono.Addins.Description;
 [assembly: Addin(
     "Meadow",
     Namespace = "WildernessLabs.Sdks",
-    Version = "0.95.0"
+    Version = "0.96.0"
 )]
 
 [assembly: AddinName("Meadow")]
