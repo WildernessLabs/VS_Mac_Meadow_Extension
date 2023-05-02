0243fcc1ae1884216e73b0d6131dd1aea5dee3b2
Dominique Louis <savagesoftware@gmail.com>
2023-05-02 16:29:34 +0100

    Bump to 0.98.0

--- VS4Mac_Meadow_Extension/Properties/AddinInfo.cs
+++ VS4Mac_Meadow_Extension/Properties/AddinInfo.cs
@@ -4,7 +4,7 @@ using Mono.Addins.Description;
 [assembly: Addin(
     "Meadow",
     Namespace = "WildernessLabs.Sdks",
-    Version = "0.97.0"
+    Version = "0.98.0"
 )]
 
 [assembly: AddinName("Meadow")]
