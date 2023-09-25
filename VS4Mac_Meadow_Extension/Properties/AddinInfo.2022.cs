26753adbaaff65fabaa2639ebe2cba18b845f1eb
Dominique Louis <savagesoftware@gmail.com>
2023-09-25 17:37:36 +0100

    Bump to 1.3.4

--- VS4Mac_Meadow_Extension/Properties/AddinInfo.2022.cs
+++ VS4Mac_Meadow_Extension/Properties/AddinInfo.2022.cs
@@ -4,7 +4,7 @@ using Mono.Addins.Description;
 [assembly: Addin(
     "Meadow.2022",
     Namespace = "WildernessLabs.Sdks",
-    Version = "1.3.0"
+    Version = "1.3.4"
 )]
 
 [assembly: AddinName("Meadow for VS2022")]
